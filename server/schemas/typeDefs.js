2f4d5a62be1ab0831913c93ce32af6b6b3493d11
bjackels5 <85116312+bjackels5@users.noreply.github.com>
2021-11-12 18:02:29 -0500

    started the typedefs and resolvers

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,33 +1,23 @@
 const { gql } = require('apollo-server-express');
 
 const typeDefs = gql`
-  type Category {
-    _id: ID
-    name: String
-  }
-
-  type Product {
-    _id: ID
-    name: String
-    description: String
-    image: String
-    quantity: Int
-    price: Float
-    category: Category
-  }
-
-  type Order {
-    _id: ID
-    purchaseDate: String
-    products: [Product]
-  }
-
   type User {
     _id: ID
     firstName: String
     lastName: String
+    userName: String
     email: String
-    orders: [Order]
+    location: String
+    bio: String
+    rightSwipes: [User]
+    githubLink: String
+    profilePicUrl: String
+  }
+
+  type Match {
+      requester: User
+      requestee: User
+      matchState: String
   }
 
   type Auth {
@@ -36,20 +26,36 @@ const typeDefs = gql`
   }
 
   type Query {
-    categories: [Category]
-    products(category: ID, name: String): [Product]
-    product(_id: ID!): Product
     user: User
-    order(_id: ID!): Order
+    matches(userId: ID): [Match]
+    rightSwipes(userId: ID): [User]
   }
 
   type Mutation {
-    addUser(firstName: String!, lastName: String!, email: String!, password: String!): Auth
-    addOrder(products: [ID]!): Order
-    updateUser(firstName: String, lastName: String, email: String, password: String): User
-    updateProduct(_id: ID!, quantity: Int!): Product
+    addUser(firstName: String!,
+            lastName: String!,
+            userName: String!, 
+            email: String!, 
+            password: String!, 
+            location: String, 
+            bio: String, 
+            githubLink: String, 
+            profilePicUrl: String): Auth
+    addRightSwipe(toAdd: ID!): User,
+    updateUser( firstName: String, 
+                lastName: String,
+                userName: String,
+                email: String, 
+                password: String, 
+                location: String, 
+                bio: String, 
+                githubLink: String, 
+                profilePicUrl: String): User
     login(email: String!, password: String!): Auth
   }
 `;
+
+
+
 
 module.exports = typeDefs;
