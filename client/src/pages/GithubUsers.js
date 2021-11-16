ea77c9ae585b2ee205bc0b93d22e73960426c422
Lightfooted <vesselofbalance@hotmail.com>
2021-11-16 12:51:42 -0500

    changed back to all users, can now display a list of 46 with username, avatar and git link

--- client/src/pages/GithubUsers.js
+++ client/src/pages/GithubUsers.js
@@ -1,31 +1,39 @@
 import React, { useState, useEffect } from "react";
 
 const GitUsers = () => {
-  const [data, setData] = useState(['']);
-  const [isLoaded, setIsLoaded] = useState(false);
+  const [data, setData] = useState([]);
+  const [searchInput, setsearchInput] = useState('');
 
   useEffect(() => {
     const fetchData = async () => {
       try {
-        const response = await fetch('https://api.github.com/search/users?q=type%3Ausa');
+        const response = await fetch('https://api.github.com/users');
+        // https://api.github.com/search/users?q=type%3Ausa
         const data = await response.json();
 
         setData(data);
         console.log(data);
-        setIsLoaded(true);
+        setsearchInput(true);
       } catch (e) {
-        setIsLoaded(true);
+        setsearchInput(true);
       }
     };
     fetchData();
   }, []);
 
-  if (!isLoaded) return <div>...loading</div>;
+  if (!searchInput) return <div>...loading</div>;
   else {
     return (
       <div className="GitUsers">
-        <h1>Fetching data by user location in USA</h1>
-        <div>{data.items[(0,29)].url}</div>
+        <h1>List of users:</h1>
+        {data.map((user) => (
+          <div>
+          <p>{user.login}</p>
+          <a href={user.url} >Github Link </a>
+          <img alt='pic' src={user.avatar_url} />
+          </div>
+        ))
+      }
       </div>
     );
   }
