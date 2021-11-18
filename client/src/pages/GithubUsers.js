64b1503d4af34d985a480ad511df35397b78b6ac
bjackels5 <85116312+bjackels5@users.noreply.github.com>
2021-11-17 23:08:11 -0500

    added my changes back in

--- client/src/pages/GithubUsers.js
+++ client/src/pages/GithubUsers.js
@@ -62,7 +62,7 @@ const GitUsers = () => {
             <div className="GitUsers">
                 <h1>List of {data.length || 0} users:</h1>
                 {data.map((user) => (
-                    <div key={user.id}>
+                    <div>
                         <p>Login ID: {user.login}, GitHub ID: {user.id} </p>
                         <a href={user.url} >Github Link </a>
                         <img alt='pic' src={user.avatar_url} />
