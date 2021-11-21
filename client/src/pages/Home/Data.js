350cfea577f22318542b074a6d727a3536492e74
brookemadison <brookeperry611@gmail.com>
2021-11-20 23:31:16 -0500

    used style components to style homepage

---
+++ client/src/pages/Home/Data.js
@@ -0,0 +1,31 @@
+export const homeObjOne = {
+    primary: true,
+    lightBg: false,
+    lightTopLine: true,
+    lightText: true,
+    lightTextDesc: true,
+    headline: 'Make the first move.',
+    buttonLabel: 'LOGIN / SIGNUP',
+    imgStart: '',
+    img: require('../../assets/cover-img.png'),
+    alt: 'man and woman looking at computer',
+    start: ''
+  };
+  
+  export const homeObjTwo = {
+    primary: true,
+    lightBg: false,
+    lightTopLine: true,
+    lightText: true,
+    lightTextDesc: true,
+    headline: "What's your Github?",
+    description:
+      "A unique experience to dating strictly for developers.",
+    buttonLabel: 'FIND YOUR LOVE MATCH',
+    imgStart: '',
+    img: require('../../assets/homepage-img.png'),
+    alt: 'woman leaning her head on another womans shoulder',
+    start: ''
+  };
+  
+  
