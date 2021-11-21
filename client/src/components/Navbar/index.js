76a1d13bb4ba78ffc8c61f39e44075cbec125d71
Padredilg <padredilg@knights.ucf.edu>
2021-11-20 19:17:29 -0500

    created hamburger menu and media queries

--- client/src/components/Navbar/index.js
+++ client/src/components/Navbar/index.js
@@ -1,27 +1,45 @@
-import React from "react";
+import React, { useState } from "react";
 import Logo from "../../assets/logo.png";
 import Auth from "../../utils/auth";
 import { Link } from "react-router-dom";
 import styled from 'styled-components';
 
+
+
 const Navbar = () => {
+    
+
     function showNavigation() {
         if (Auth.loggedIn()) {
             return (
-                <div>
-                    <Link to="/profile">Profile</Link>
-                    <Link to="/gitusers">Explore</Link>
-                    <Link to="/findmatch">Find-match</Link>
-                    <Link to="/chat">Chat</Link>
-                    <Link to="/" onClick={() => Auth.logout()}>Logout</Link>
-                </div>
+                <Menu>
+                    <Link className="my-1" to="/profile">
+                        <MenuLink>Profile</MenuLink>
+                    </Link>
+                    <Link className="my-1" to="/gitusers">
+                        <MenuLink>Explore</MenuLink>
+                    </Link>
+                    <Link className="my-1" to="/findmatch">
+                        <MenuLink>Find-match</MenuLink>
+                    </Link>
+                    <Link className="my-1" to="/chat">
+                        <MenuLink>Chat</MenuLink>
+                    </Link>
+                    <Link className="my-1" to="/" onClick={() => Auth.logout()}>
+                        <MenuLink>Logout</MenuLink>
+                    </Link>
+                </Menu>
             );
         } else {
             return (
-                <div>
-                    <Link to="/signup">Signup</Link>
-                    <Link to="/login">Login</Link>
-                </div>
+                <Menu>
+                    <Link className="my-1" to="/signup">
+                        <MenuLink>Signup</MenuLink>
+                    </Link>
+                    <Link className="my-1" to="/login">
+                        <MenuLink>Login</MenuLink>
+                    </Link>
+                </Menu>
             );
         }
     }
@@ -37,25 +55,72 @@ const Navbar = () => {
                 <span />
                 <span />
                 <span />
-                <span />
             </Hamburger>
-            <Menu>
-                {showNavigation()}
-            </Menu>
+
+            {showNavigation()}
+
         </Nav>
     );
 }
 
 const Nav = styled.div`
-    padding: 0 2rem;
+    text-transform: uppercase;
+    padding: 1rem 2rem;
     display: flex;
     justify-content: space-between;
+    flex-wrap: wrap;
+    background: none;
 `;
 
-const Hamburger = styled.div``;
+const Hamburger = styled.div`
+    display: none;
+    flex-direction: column;
+    padding-top: .75rem;
 
-const Menu = styled.div``;
+    span{
+        height: 2.5px;
+        width: 25px;
+        background: #2C6975;
+        margin-bottom: 4px;
+        border-radius: 5px;
+        transition: all .3s ease-in-out;
+    }
 
+    &:hover{
+        span{
+            background: #68B2A0;
+        }
+    }
+
+    @media (max-width:768px){
+        display: flex;
+    }
+`;
+
+const Menu = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    position: relative;
+
+    @media (max-width:768px){
+        overflow: hidden;
+        flex-direction: column;
+        width: 100%;
+    }
+`;
+
+const MenuLink = styled.a`
+    padding: 1rem .8rem;
+    transition: all 0.3s ease-in-out;
+    font-size: 0.9rem;
+    text-align: center;
+
+
+    &:hover{
+        color: orange;
+    }
+`
 // const MenuLink = styled.div``;
 
 export default Navbar;
