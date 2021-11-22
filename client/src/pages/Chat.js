969779c26f1f5c55e5d602ad6310c47ee760baca
Lightfooted <vesselofbalance@hotmail.com>
2021-11-22 17:01:47 -0500

    attempting merge

--- client/src/pages/Chat.js
+++ client/src/pages/Chat.js
@@ -1,35 +1,35 @@
-import React, { useEffect } from 'react';
-import { Widget, addResponseMessage } from 'react-chat-widget';
-import { QUERY_USER } from '../utils/queries';
-import { useQuery } from '@apollo/client';
+// import React, { useEffect } from 'react';
+// import { Widget, addResponseMessage } from 'react-chat-widget';
+// import { QUERY_USER } from '../utils/queries';
+// import { useQuery } from '@apollo/client';
 
-// We do not have to use the default css. Create a new styles.css file. Keep in mind though that all classes are prefixed as rcw-
-import 'react-chat-widget/lib/styles.css';
-import UPIC from '../assets/genericuser.png';
+// // We do not have to use the default css. Create a new styles.css file. Keep in mind though that all classes are prefixed as rcw-
+// import 'react-chat-widget/lib/styles.css';
+// import UPIC from '../assets/genericuser.png';
 
-function Chat() {
+// function Chat() {
 
-  const { data } = useQuery(QUERY_USER);
+//   const { data } = useQuery(QUERY_USER);
 
-  useEffect(() => {
-    addResponseMessage('Welcome back!');
-  }, []);
+//   useEffect(() => {
+//     addResponseMessage('Welcome back!');
+//   }, []);
 
-  const handleNewUserMessage = () => {
-  };
+//   const handleNewUserMessage = () => {
+//   };
 
-  const user = data?.user || {};
+//   const user = data?.user || {};
 
-    return (
-      <div className="App">
-        <Widget
-          handleNewUserMessage={handleNewUserMessage}
-          titleAvatar={UPIC}
-          title={user.firstName}
-          subtitle='HTML Chat'
-        />
-      </div>
-    );
-}
+//     return (
+//       <div className="App">
+//         <Widget
+//           handleNewUserMessage={handleNewUserMessage}
+//           titleAvatar={UPIC}
+//           title={user.firstName}
+//           subtitle='HTML Chat'
+//         />
+//       </div>
+//     );
+// }
 
-export default Chat;
+// export default Chat;
